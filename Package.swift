a93f81b62e3cafb0b1e2b8919ed85a204b987713
Mox Soini <mox.soini@fjordnet.com>
2016-07-03 20:49:15 +0300

    Add Vapor as dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "ServerSwift"
+    name: "ServerSwift",
+    dependencies: [
+      .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12)
+  ]
 )
