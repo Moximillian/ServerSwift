7cbdcf6d4d3dcb7b9d24e4652eacefaa3b781602
Mox Soini <mox.soini@fjordnet.com>
2016-07-04 01:35:39 +0300

    Serve a struct as json

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,28 @@
 import Vapor
+import Foundation
+
+struct Stuff {
+  let index: Int = 9
+  let title: String = "Foober"
+
+  func toJSON() -> String {
+    let dict = ["index": index, "title": title]
+    do {
+      let data = try NSJSONSerialization.data(withJSONObject: dict)
+      return String(data: data, encoding: NSUTF8StringEncoding) ?? ""
+    } catch let error {
+      print("error converting to json: \(error)")
+      return ""
+    }
+  }
+}
+
+let s = Stuff()
 
 let app = Application()
 
 app.get("/") { request in
-  return "Hello swift!"
+  return s.toJSON()
 }
 
 app.start()
