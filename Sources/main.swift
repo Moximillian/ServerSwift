c27a9dac598210fad1437c29bc64054c019475fb
Mox Soini <mox.soini@fjordnet.com>
2016-10-08 21:59:15 +0300

    Swift 3.0

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,14 +9,14 @@ struct Stuff {
   func toJSON() -> String {
     // Foundation on Linux is crap
     #if os(OSX) || os(iOS)
-      let dict = ["index": index, "title": title]
+      let dict: [String: Any] = ["index": index, "title": title]
     #else
       let putput: [String: AnyObject] = ["index": NSNumber(value: index), "title": title.bridge() as NSString]
       let dict: NSDictionary = putput.bridge()
     #endif
     do {
-      let data = try NSJSONSerialization.data(withJSONObject: dict)
-      return String(data: data, encoding: NSUTF8StringEncoding) ?? ""
+      let data = try JSONSerialization.data(withJSONObject: dict)
+      return String(data: data, encoding: String.Encoding.utf8) ?? ""
     } catch let error {
       print("error converting to json: \(error)")
       return ""
