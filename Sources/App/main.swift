6f06aef086ac9fb05e534349b86bcb1432f53a8f
Mox Soini <mox@iki.fi>
2017-07-14 00:15:48 +0300

    Use encoder from Foundation

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,31 +1,21 @@
 import Foundation
 import Vapor
-//import Gloss
 
-struct Stuff {
+struct Stuff: Codable {
   let index: Int = 9
   let title: String = "Foober"
-
-  func toJSON() -> String {
-    let dict: [String: Any] = ["index": index, "title": title]
-    do {
-      let data = try JSONSerialization.data(withJSONObject: dict)
-      return String(data: data, encoding: String.Encoding.utf8) ?? ""
-    } catch let error {
-      print("error converting to json: \(error)")
-      return ""
-    }
-  }
 }
 
 let s = Stuff()
+let encoder = JSONEncoder()
 
 do {
   let drop = try Droplet()
 
   drop.get("/") { request in
     // print("REQUEST: ", request)
-    return s.toJSON()
+    let data = try encoder.encode(s)
+    return String(data: data, encoding: .utf8) ?? ""
   }
   
   try drop.run()
