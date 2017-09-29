02619be8410597410a33f1ba0f9da047c8015ab9
Mox Soini <mox@iki.fi>
2017-09-29 11:40:45 +0300

    Use proper app name

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .package(url: "https://github.com/vapor/vapor", from: "2.2.0")
   ],
   targets: [
-    .target(name: "App", dependencies: ["Vapor"], path: ".", sources: ["Sources"])
+    .target(name: "ServerSwift", dependencies: ["Vapor"], path: ".", sources: ["Sources"])
   ],
   // "products: " -definition is implicit based on targets
   swiftLanguageVersions: [4]
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: App --env=production --workdir=./ --config:server.port=$PORT
+web: ServerSwift --env=production --workdir=./ --config:server.port=$PORT
